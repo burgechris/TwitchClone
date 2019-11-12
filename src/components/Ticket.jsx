fc4b71421ac4840282f51cfec6d57b643ba47cf5
Devin Cooley <dcooley1350@gmail.com>
2019-11-11 23:35:32 -0800

    add propTypes, project now at Building an Environment

--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -1,14 +1,19 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function Ticket(){
+function Ticket(props){
     return (
         <div>
-            <h3>3a</h3>
-            <h3>Thato and Haley</h3>
-            <p><em>Firebase will not save record!</em></p>
+            <h3>{props.location} - {props.names}</h3>
+            <p><em>{props.issue}</em></p>
             <hr/>
         </div>
     );
 }
+ Ticket.propTypes = {
+     names: PropTypes.string.isRequired,
+     location: PropTypes.string.isRequired,
+     issue: PropTypes.string
+ }
 
 export default Ticket;
